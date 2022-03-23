c7b994562d148f0fcbb30a8b5d276c93cb99377c
suleevn <suleevn@gmail.com>
2022-03-23 21:41:10 +0600

    1.3

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -90,7 +90,7 @@ AUTHENTICATION_BACKENDS = (
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
