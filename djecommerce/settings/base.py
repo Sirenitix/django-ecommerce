a65d96acb62fec7b3d681e6a26c149dc3b360efd
Sirenitix <suleevn@gmail.com>
2022-06-01 14:26:23 +0600

    Update base.py

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -6,7 +6,7 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
-SECRET_KEY = config('SECRET_KEY')
+
 
 DATABASE_ROUTERS = ['djecommerce.PrimaryReplicaRouter.PrimaryReplicaRouter']
 
