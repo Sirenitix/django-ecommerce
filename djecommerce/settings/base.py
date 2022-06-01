1756df855c02f769121ff009d0a7249b5448653a
Sirenitix <suleevn@gmail.com>
2022-06-01 22:05:23 +0600

    Update base.py

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -92,10 +92,10 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sirenitix@gmail.com'
-EMAIL_HOST_PASSWORD = 'Popidenil#1'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'suleevn@gmail.com'
+EMAIL_HOST_PASSWORD = 'tchomujyyxycbiuz'
 DEFAULT_FROM_EMAIL = 'From Hardware Store'
 
 # CRISPY FORMS
