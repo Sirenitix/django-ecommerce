37cc984ce8362b98f2da688ce1a5cb801b5de8ce
Sirenitix <suleevn@gmail.com>
2022-06-01 15:24:09 +0600

    Update base.py

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -1,16 +1,12 @@
 import os
-import environ
-
-env = environ.Env()
-
-# read th .env file
-environ.Env.read_env()
+from dotenv import load_dotenv
+load_dotenv()
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 DATABASE_ROUTERS = ['djecommerce.PrimaryReplicaRouter.PrimaryReplicaRouter']
 
